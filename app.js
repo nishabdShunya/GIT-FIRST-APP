d4c3b24b788982dab9ee9e18292c8a84aa989571
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-17 02:29:13 +0530

    routing requests: basically connecting the request with response

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    if(req.url === '/home'){
-        res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    if (url === '/') {
         res.write('<html>');
         res.write('<meta><title>My First Page</title></meta>');
-        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
         res.write('</html>');
         return res.end();
     }
-    else if (req.url === '/about'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<meta><title>My First Page</title></meta>');
-        res.write('<body><h1>Welcome to About Us Page</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if (req.url === '/node'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<meta><title>My First Page</title></meta>');
-        res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<meta><title>My First Page</title></meta>');
+    res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
+    res.write('</html>');
+    return res.end();
 });
 
 server.listen(3000);
