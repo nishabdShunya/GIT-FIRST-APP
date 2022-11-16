9021c374367777d74ab535fcaddf43aaa3dbd655
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-17 01:09:00 +0530

    changed response for different urls

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<meta><title>My First Page</title></meta>');
-    res.write('<body><h1>Welcome to my Node.js Server!</h1></body>');
-    res.write('</html>');
-    return res.end();
+    if(req.url === '/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<meta><title>My First Page</title></meta>');
+        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (req.url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<meta><title>My First Page</title></meta>');
+        res.write('<body><h1>Welcome to About Us Page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (req.url === '/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<meta><title>My First Page</title></meta>');
+        res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 server.listen(3000);
