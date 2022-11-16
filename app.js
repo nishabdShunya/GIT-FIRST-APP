7e8624a952d4bc59e18d59d21ed515974026d0bc
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-17 01:03:05 +0530

    understood request object and how to send back the response from the server

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-    console.log('Nishant');
-})
-server.listen(4000);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<meta><title>My First Page</title></meta>');
+    res.write('<body><h1>Welcome to my Node.js Server!</h1></body>');
+    res.write('</html>');
+    return res.end();
+});
+
+server.listen(3000);
