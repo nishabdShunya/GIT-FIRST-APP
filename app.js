dc854d8b5852f7005738c66840f37d70c8206058
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-16 13:17:59 +0530

    imported the http module, created a server and listened to port 4000

---
+++ app.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('Nishant');
+})
+server.listen(4000);
--- first-app.js
+++ first-app.js
@@ -1,5 +1,2 @@
 const fs = require('fs');
-
 fs.writeFileSync('hello.txt', 'Hello from Node.js');
-
-console.log('Hello World');
