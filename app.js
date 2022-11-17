bd00683e3c0b4be6b71612db67c978094469dc21
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-17 12:16:07 +0530

    read the file and displayed data on screen

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if (url === '/') {
-        res.write('<html>');
-        res.write('<meta><title>My First Page</title></meta>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
-        res.write('</html>');
-        return res.end();
+        return fs.readFile('message.txt', { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            res.write('<html>');
+            res.write('<meta><title>My First Page</title></meta>');
+            res.write(`<body>${data}</body>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        })
     }
-    if (url === '/message' && method === 'POST') {
+    else if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
             console.log(chunk);
@@ -21,6 +27,9 @@ const server = http.createServer((req, res) => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.log(err);
+                }
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
                 return res.end();
